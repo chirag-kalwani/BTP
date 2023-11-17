068597e50531b8208025247224f2983d0fc39bf9
Prashantsgh <106375453+Prashantsgh@users.noreply.github.com>
2023-11-17 20:59:53 +0530

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,2 +1,3 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default "http://localhost:8000";
+// export default "http://localhost:8000";
+export default "https://final-btp.onrender.com";
