049f9f4e07e7f3abff857886da0b1f54bd35b4a7
KartikGupta12 <kartikguptamzn2001@gmail.com>
2023-11-17 23:38:30 +0530

    scanner updated

--- src/components/BarcodeScanner.js
+++ src/components/BarcodeScanner.js
@@ -52,18 +52,25 @@ function BarcodeScanner(props) {
 
     const initScanner = () => {
         let wid=document.getElementById('scanbutton');
-        let hieght=(window.innerWidth>1000)?530:192
+        let hieght=(window.innerWidth>1000)?530:400
         Quagga.init({
             inputStream: {
                 name: 'Live',
                 type: 'LiveStream',
                 target: document.querySelector('#scanner-container'),
                 constraints: {
-                    width: wid.clientWidth,
+                    width:wid.clientWidth,
                     height: hieght,
                     facingMode: 'environment', // Use the device's front camera
                 },
             },
+            locator: {
+                "patchSize": "medium",
+                "halfSample": true
+              },
+              "locate": true,
+              "numOfWorkers": 2,
+              "frequency": 10,
             decoder: {
                 readers: ["code_128_reader",
                 "ean_reader",
@@ -83,7 +90,47 @@ function BarcodeScanner(props) {
             }
             Quagga.start();
         });
+        // Quagga.onProcessed(result => {
+        //     var drawingCtx = Quagga.canvas.ctx.overlay,
+        //       drawingCanvas = Quagga.canvas.dom.overlay;
 
+        //     if (result) {
+        //       if (result.boxes) {
+        //         drawingCtx.clearRect(
+        //           0,
+        //           0,
+        //           Number(drawingCanvas.getAttribute("width")),
+        //           Number(drawingCanvas.getAttribute("height"))
+        //         );
+        //         result.boxes
+        //           .filter(function(box) {
+        //             return box !== result.box;
+        //           })
+        //           .forEach(function(box) {
+        //             Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+        //               color: "green",
+        //               lineWidth: 2
+        //             });
+        //           });
+        //       }
+
+        //       if (result.box) {
+        //         Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+        //           color: "#00F",
+        //           lineWidth: 2
+        //         });
+        //       }
+
+        //       if (result.codeResult && result.codeResult.code) {
+        //         Quagga.ImageDebug.drawPath(
+        //           result.line,
+        //           { x: "x", y: "y" },
+        //           drawingCtx,
+        //           { color: "red", lineWidth: 3 }
+        //         );
+        //       }
+        //     }
+        //   });
         Quagga.onDetected((result) => {
             const barcodeNumber = result.codeResult.code;
             // setbarcode(barcodeNumber);
@@ -127,6 +174,7 @@ function BarcodeScanner(props) {
                 {isScannerOpen ? 'Close Scanner' : 'Open Scanner'}
             </button>
             <div id="scanner-container"></div>
+            {/* <div id="interactive" className="viewport" /> */}
 
         </div>
     );
