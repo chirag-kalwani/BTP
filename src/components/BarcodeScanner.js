cccb622040e68c320fc8bc16a840ed8c7ff46db9
KartikGupta12 <kartikguptamzn2001@gmail.com>
2023-11-17 18:46:54 +0530

    Update BarcodeScanner.js

--- src/components/BarcodeScanner.js
+++ src/components/BarcodeScanner.js
@@ -1,15 +1,11 @@
 import React, {useState} from 'react';
 import Quagga from 'quagga';
-
-
 import { useEffect } from 'react';
 import useWindowSize from '../Hooks/useWindowSize';
 function BarcodeScanner(props) {
     const [isScannerOpen, setIsScannerOpen] = useState(true);
-
-    const [productname, setproductname] = useState("");
     const [width] = useWindowSize();
-    //const [width] = useWindowSize();
+
     const handlesave = (b) => {
         const apiUrl = `https://world.openfoodfacts.net/api/v2/product/${b}?fields=product_name,nutriments,categories_tags_en`;
 
@@ -27,41 +23,32 @@ function BarcodeScanner(props) {
             .catch((err) => {
                 console.log(err);
             })
-        // props.scanning({
-        //   "product": {
-        //     "nutriments": {
-        //       "carbohydrates": 57.5,
-        //       "carbohydrates_100g": 57.5,
-        //       "carbohydrates_unit": "g",
-        //       "carbohydrates_value": 57.5,
-        //       "energy": 2255,
-        //       "energy-kcal": 539,
-        //       "energy-kcal_100g": 539,
-        //       "energy-kcal_unit": "kcal",
-        //       "sugars": 56.3,
-        //       "sugars_100g": 56.3,
-        //       "sugars_unit": "g",
-        //       "sugars_value": 56.3
-        //     },
-        //     "nutriscore_data": {
-        //       "energy": 2255,
-        //       "energy_points": 6,
-        //       "energy_value": 2255,
-        //       "sugars_points": 10,
-        //       "sugars_value": 56.3
-        //     },
-        //     "nutrition_grades": "e",
-        //     "product_name": "Nutella"
-        //   },
-        //   "status": 1,
-        //   "status_verbose": "product found"
-        // })
+
     }
 
-useEffect(() => {
 
-  initScanner();
-}, [width])
+
+    useEffect(() => {
+
+      initScanner();
+      setIsScannerOpen(true);
+    }, [width]);
+
+    const closeCam = () => {
+        // const video = document.querySelector('#scanner-container video');
+        // const tracks=video.srcObject.getTracks();
+        // console.log(tracks);
+        // tracks.forEach((track) => {track.stop();
+        // });
+        // video.src='';
+        //
+        mediastreamtrack.stop();
+
+        // console.log(tracks);
+    //     const video = document.querySelector('#scanner-container video');
+    //     window.localStream.getVideoTracks()[0].stop();
+    // video.src = '';
+    }
 
     const initScanner = () => {
         let wid=document.getElementById('scanbutton');
@@ -74,11 +61,20 @@ useEffect(() => {
                 constraints: {
                     width: wid.clientWidth,
                     height: hieght,
-                    facingMode: 'user', // Use the device's rear camera
+                    facingMode: 'user', // Use the device's front camera
                 },
             },
             decoder: {
-                readers: ["ean_reader"], // Change the reader type according to your barcode format
+                readers: ["code_128_reader",
+                "ean_reader",
+                "ean_8_reader",
+                "code_39_reader",
+                "code_39_vin_reader",
+                "codabar_reader",
+                "upc_reader",
+                "upc_e_reader",
+                "i2of5_reader"], // Change the reader type according to your barcode format
+
             },
         }, function (err) {
             if (err) {
@@ -92,6 +88,7 @@ useEffect(() => {
             const barcodeNumber = result.codeResult.code;
             // setbarcode(barcodeNumber);
             Quagga.stop();
+            closeCam();
             setIsScannerOpen(false);
             // console.log(barcode);
             handlesave("3017624010701");
@@ -100,19 +97,23 @@ useEffect(() => {
             // You can now use the barcodeNumber as needed in your application.
         });
 
+
         return () => {
             Quagga.stop();
-
+            isScannerOpen(false);
+            closeCam();
         };
-    }
+    };
+
 
     const toggleScanner = () => {
         if (isScannerOpen) {
             Quagga.stop();
+            closeCam();
+
         } else {
             initScanner();
         }
-        console.log("heelo");
         setIsScannerOpen(!isScannerOpen);
     };
 
@@ -133,87 +134,5 @@ useEffect(() => {
 }
 
 export default BarcodeScanner;
-
-
-// // //import React from 'react'
-// import React, { useEffect, useState } from 'react';
-// import Quagga from 'quagga';
-// function BarcodeScanner() {
-//    // const [barcode, setbarcode] = useState();
-//     const [istrue, setistrue] = useState(false);
-//     const [productname, setproductname] = useState("");
-//     const handlesave=(b)=>{
-//         const apiUrl = `https://world.openfoodfacts.net/api/v2/product/${b}?fields=product_name`;
-
-
-//     fetch(apiUrl)
-//       .then((response) => {
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return response.json();
-//       })
-//       .then((data) => {
-//         setproductname(data.product.product_name);
-//         console.log(data.product.product_name);
-//         setistrue(true);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       })
-//     }
-
-
-//     useEffect(() => {
-//       Quagga.init({
-//         inputStream: {
-//           name: 'Live',
-//           type: 'LiveStream',
-//           target: document.querySelector('#scanner-container'),
-//           constraints: {
-//             width: 640,
-//             height: 480,
-//             facingMode: 'environment', // Use the device's rear camera
-//           },
-//         },
-//         decoder: {
-//             readers : ["ean_reader"], // Change the reader type according to your barcode format
-//         },
-//       }, function (err) {
-//         if (err) {
-//           console.error(err);
-//           return;
-//         }
-//         Quagga.start();
-//       });
-
-//       Quagga.onDetected((result) => {
-//         const barcodeNumber = result.codeResult.code;
-//        // setbarcode(barcodeNumber);
-//         Quagga.stop();
-//        // console.log(barcode);
-//         handlesave(barcodeNumber);
-//         console.log('Detected barcode: ' + barcodeNumber);
-
-//         // You can now use the barcodeNumber as needed in your application.
-//       });
-
-//       return () => {
-//         Quagga.stop();
-
-//       };
-//     }, []);
-
-//     return (
-//       <div>
-//         <div id="scanner-container"></div>
-//         {/* <button onClick={handlesave}>getproduct</button> */}
-//         {<div>{productname}</div>}
-//       </div>
-//     );
-//   }
-
-//   export default BarcodeScanner;
-
 
 
